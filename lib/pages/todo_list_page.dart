6bfbbe6a5d9e28bb5dd9b0182bd9d40ff70cd9a7
Murilo Ossanes <murilossanes@hotmail.com>
2026-01-21 17:47:43 -0300

    todo_list

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                onChanged: onChanged,
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: Text('Entrar'))
+            ],
+          ),
+        ),
+        ),
+        );
+  }
+
+  void login(){
+    String text = emailController.text;
+    print(text);
+  }
+
+  void onChanged(String text){
+    print(text);
+  }
+}
